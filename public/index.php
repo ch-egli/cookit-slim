87b081c7ca90bf0628ff287e94927df9de1e6b50
ch-egli <chrsitian.egli@gmx.net>
2020-12-31 17:10:08 +0100

    Adapt path of index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/config/DotEnv.php';
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/src/config/DotEnv.php';
 
 // # include DB connection file
 require '../src/config/Database.php';
@@ -10,10 +10,24 @@ require '../src/config/Database.php';
 $app = AppFactory::create();
 $app->addBodyParsingMiddleware();
 
+// Enable CORS
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    $response = $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', '*')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+    return $response;
+});
+
 // # include cookit routes
-require '../src/repo/RecipeRepo.php';
-require '../src/helper/Authentication.php';
-require '../src/helper/JsonResponse.php';
-require '../src/routes/cookit-routes.php';
+require 'src/repo/RecipeRepo.php';
+require 'src/helper/Authentication.php';
+require 'src/helper/JsonResponse.php';
+require 'src/routes/cookit-routes.php';
 
 $app->run();
